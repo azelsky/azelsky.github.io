eee2638654e87df30b24c1c803dea988f945cded
azelsky <andry.zelinsky@gmail.com>
2018-06-05 18:41:58 +0300

    waxom

---
+++ waxom/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		scrollBar: false,
+		controlArrows: true,
+		navigation: false,
+		slidesNavigation: true,
+		slidesNavPosition: "bottom",
+	});
+});
