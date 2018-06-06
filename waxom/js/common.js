f5cd5b601a7a1a0d910a4d5625871443978fefef
azelsky <andry.zelinsky@gmail.com>
2018-06-06 19:34:18 +0300

    waxom 6/06/18

--- waxom/js/common.js
+++ waxom/js/common.js
@@ -6,4 +6,15 @@ $(document).ready(function() {
 		slidesNavigation: true,
 		slidesNavPosition: "bottom",
 	});
+
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
